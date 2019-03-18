fd07f9f68c6e6c490577a110798cf9ec74d567aa
wengzhenquan <wzq12648103>
2019-03-18 14:07:49 +0800

    构建项目

---
+++ src/main/java/com/wzq/algorithmexercises/AlgorithmExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.wzq.algorithmexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmExercisesApplication.class, args);
+    }
+
+}
